777fa7ebcce10509e9c3c414e83a89d9d05a2b74
Eric <eric.d.s.info@gmail.com>
2020-08-25 10:50:46 +0100

    Advanced the iterator explanations

---
+++ tests/blocks.rb
@@ -0,0 +1,8 @@
+puts 5 + 6
+
+# Vamos a refactorizarlo para que emplee variables:
+a = 5
+b = 6
+puts a + b
+
+# El siguiente paso es hacer que esa sección de código esté almacenado en una variable:
